ee130e52c7554b649e9b260351f5f3e23033f7bc
TuanNguyen <ndtuan.bkdn@gmail.com>
2021-09-10 14:57:38 +0700

    1

--- src/Component/GetStartedComponent/GetStartedComponent.js
+++ src/Component/GetStartedComponent/GetStartedComponent.js
@@ -8,7 +8,6 @@ export default class GetStartedComponent extends Component {
                     <h4 style={{color:'#fff',lineHeight:'50px'}}>Become a part of Linexon bussiness community today</h4>
                     <button style={{border:'1px solid #fff',backgroundColor:'blue',color:'#fff', padding:'10px', borderRadius:'5px'}}>GET STARTED</button>
                 </div>
-                
             </div>
         )
     }
